8917c646bf9af8e9139b83800c6ab1b884176a69
Duy Tan Tran <duy.tan.huynh.tran@dnv.com>
2022-06-14 14:59:59 +0200

    remove tryInsert

--- src/IngestApi/DbInitService.cs
+++ src/IngestApi/DbInitService.cs
@@ -15,7 +15,6 @@ public sealed class DbInitService : IHostedService
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         await TryInit(cancellationToken);
-        await TryInsert(cancellationToken);
     }
 
     private async Task TryInit(CancellationToken cancellationToken)
@@ -23,39 +22,5 @@ public sealed class DbInitService : IHostedService
         await _repository.Initialize(cancellationToken);
     }
 
-    private async Task TryInsert(CancellationToken cancellationToken)
-    {
-        var dataChannelListDto = await GetDataChannelList(cancellationToken);
-
-        await _repository.InsertDataChannel(dataChannelListDto, cancellationToken);
-    }
-
-
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-    // This is just for testing json data
-    async Task<DataChannelListPackage> GetDataChannelList(CancellationToken stoppingToken)
-    {
-        Stream GetResource(string name)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var names = assembly.GetManifestResourceNames();
-            var resourceName = names.FirstOrDefault(n => n.EndsWith(name.Replace("/", ".")));
-            if (resourceName is null)
-                throw new Exception("Couldnt find file");
-            return Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)
-                ?? throw new Exception("Couldnt find file");
-        }
-
-        await using var dataChannelsFile = GetResource("resources/DataChannelList.json");
-
-        var dataChannelListDto = await Serializer.DeserializeDataChannelListAsync(
-            dataChannelsFile,
-            stoppingToken
-        );
-        if (dataChannelListDto is null)
-            throw new Exception("Couldnt load datachannels");
-
-        return dataChannelListDto;
-    }
 }
